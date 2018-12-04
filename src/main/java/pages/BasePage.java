f68d93cfaf2c86bd50c92bfdba48e0a44c01b0be
sergeyl <sergeyl@123software.ru>
2018-12-04 15:04:35 +0200

    fix

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -9,11 +9,7 @@ public class BasePage {
 
     public void open(){
         Selenide.open(pageUrl);
-//        WebDriverRunner.url()
-    }
-
-    public boolean isPageOpen(){
-        return WebDriverRunner.url().equals(pageUrl);
+        assert  WebDriverRunner.url().equals(pageUrl);
     }
 
 }
